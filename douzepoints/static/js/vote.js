933572226c87598d35bee075df53bec9470860b8
daniel <>
2021-04-16 21:27:18 +0200

    Vote reload

--- douzepoints/static/js/vote.js
+++ douzepoints/static/js/vote.js
@@ -7,65 +7,65 @@ $(document).ready(function() {
     // This code used for set order attribute for items
     var numberOfItems = $("#options").find('li').length;
     $.each($("#options").find('li'), function(index, item) {
-      $(item).attr("order", index);
-      var removeBotton = $('<div class="remove" style="display:none">&times</div>');
-      removeBotton.click(function() {
-        resetPosition($(this).parent());
-      });
-      $(item).append(removeBotton);
+        $(item).attr("order", index);
+        var removeBotton = $('<div class="remove" style="display:none">&times</div>');
+        removeBotton.click(function() {
+            resetPosition($(this).parent());
+        });
+        $(item).append(removeBotton);
   
     });
   
     $(".points").droppable({
-      accept: "li",
-      classes: {
-        "ui-droppable-hover": "ui-state-hover"
-      },
-      drop: function(event, ui) {
-        // Check for existing another option
-        if ($(this).find('li').length > 0)
-          resetPosition($(this).find('li'));
+        accept: "li",
+        classes: {
+            "ui-droppable-hover": "ui-state-hover"
+        },
+        drop: function(event, ui) {
+            // Check for existing another option
+            if ($(this).find('li').length > 0)
+                resetPosition($(this).find('li'));
   
-        $(ui.draggable).find('div.remove').attr("style", "");
-        $(this).append($(ui.draggable));
-  
-      }
+            $(ui.draggable).find('div.remove').attr("style", "");
+            $(this).append($(ui.draggable));
+        }
     });
   
     $("li").draggable({
-      helper: "clone",
-      revert: "invalid"
+        helper: "clone",
+        revert: "invalid"
     });
   
   
     // This function used for find old place of item
     function resetPosition($item) {
-      var indexItem = $item.attr('order');
-      var itemList = $("#options").find('li');
-      $item.find('div.remove').hide();
+        var indexItem = $item.attr('order');
+        var itemList = $("#options").find('li');
+        $item.find('div.remove').hide();
   
-      if (indexItem === "0")
-        $("#options").prepend($item);
-      else if (Number(indexItem) === (Number(numberOfItems) - 1))
-        $("#options").append($item);
-      else
-        $(itemList[indexItem - 1]).after($item);
-    }
-  
-      $("button").click(function() {
-        vote = {};
-        $.each($(".points"), function() {
-            if ( $(this).find("li").html() != null) {
-            vote[$(this).prev().prev().attr("id")] = $(this).find("li").contents().filter((i, el) => el.nodeType === 3).text();
-        } else {
-          // TODO: Check for points not assigned!!!
-            // alert("No points assigned to: " + $(this).prev().prev().text());
+        if (indexItem === "0")
+            $("#options").prepend($item);
+        else if (Number(indexItem) === (Number(numberOfItems) - 1))
+            $("#options").append($item);
+        else
+            $(itemList[indexItem - 1]).after($item);
         }
-      });
-      vname = $("#name").val();
-      socketio.emit("vote", urlParams.get("cid"), vname, vote);
+  
+        $("button").click(function() {
+            vote = {};
+            $.each($(".points"), function() {
+            if ( $(this).find("li").html() != null) {
+                vote[$(this).prev().prev().attr("id")] = $(this).find("li").contents().filter((i, el) => el.nodeType === 3).text();
+            } else {
+                // TODO: Check for points not assigned!!!
+                // alert("No points assigned to: " + $(this).prev().prev().text());
+            }
+        });
+        vname = $("#name").val();
+        socketio.emit("vote", urlParams.get("cid"), vname, vote);
+        location.reload();
     });
-  })
+})
   
   
   
